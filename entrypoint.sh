1f4f9bf73688b989316b539d821de9a89cea4ecd
Spinel24x <spinelapper@gmail.com>
2026-08-04 14:40:52 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-/usr/local/bin/xray -config /usr/local/etc/xray/config.json &
+
+# مسیر Xray دستی نصب شده
+/usr/local/xray/xray -config /usr/local/etc/xray/config.json &
+
+# پنل مدیریت
 python3 /app.py
