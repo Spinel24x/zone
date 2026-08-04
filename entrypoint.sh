9a56186bc22e4cdfa4916253e234bb92ff6841e7
Spinel24x <spinelapper@gmail.com>
2026-08-04 14:57:27 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# مسیر Xray دستی نصب شده
+# اجرای Xray روی پورت 8081 (داخلی)
 /usr/local/xray/xray -config /usr/local/etc/xray/config.json &
 
-# پنل مدیریت
+# اجرای Flask روی پورت 8080 (عمومی برای Railway)
 python3 /app.py
